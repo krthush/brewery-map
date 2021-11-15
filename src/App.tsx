10edf18a7a02e7ba4a4383752c8a72ba61b5ac8c
krthush <33790334+krthush@users.noreply.github.com>
2021-11-15 10:55:36 +0000

    added react leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Map from './Map';
+
 function App() {
   return (
     <div className="app">
       <main className="main">
-      <h1>Welcome to <a href="https://github.com/krthush/brewery-map">Brewery Map!</a></h1>
-      <h2>An interactive map for fetching Breweries from the Open Brewery API.</h2>
+        <h1>Welcome to <a href="https://github.com/krthush/brewery-map">Brewery Map!</a></h1>
+        <h2>An interactive map for fetching Breweries from the Open Brewery API.</h2>
+        <Map></Map>
       </main>
     </div>
   );
