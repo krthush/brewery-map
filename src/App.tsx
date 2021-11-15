39af3c5d446c7eb1212f883070270ea40a2a3f2c
krthush <33790334+krthush@users.noreply.github.com>
2021-11-15 10:19:03 +0000

    project setup + remove redundant libs/files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main className="main">
+      <h1>Welcome to <a href="https://github.com/krthush/brewery-map">Brewery Map!</a></h1>
+      <h2>An interactive map for fetching Breweries from the Open Brewery API.</h2>
+      </main>
     </div>
   );
 }
