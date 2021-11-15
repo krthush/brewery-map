3c5c010d0a15a6955acff07a99c1086246ddc01a
krthush <33790334+krthush@users.noreply.github.com>
2021-11-15 11:08:49 +0000

    using mapbox for better looking maps
    see https://www.freecodecamp.org/news/how-to-set-up-a-custom-mapbox-basemap-with-gatsby-and-react-leaflet/

--- src/Map.tsx
+++ src/Map.tsx
@@ -8,9 +8,10 @@ function Map() {
   return (
     <div>
       <MapContainer center={position} zoom={13} className="map-container">
+        {/* TODO: Don't reveal MAPBOX token here user */}
         <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url={`https://api.mapbox.com/styles/v1/krthush/ckw0k00ne04jv16qxocrdtkcr/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+          attribution="Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>"
         />
         <Marker position={position}>
           <Popup>
